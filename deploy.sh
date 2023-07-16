d8d9d31d515c2206bb08ad14f8534464f836a3e2
Kim Quibral <kimquibral2000@gmail.com>
2023-07-16 13:35:46 +0800

    isPending to isDelivered

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,25 @@
 read -p "Enter commit message: " commit_message
+echo "=================================================="
+echo "Running flutter build web --release ..."
+echo "=================================================="
 flutter build web --release
+
+echo "=================================================="
+echo "Running firebase deploy ..."
+echo "=================================================="
 firebase deploy
+
+echo "=================================================="
+echo "Running git add . ..."
+echo "=================================================="
 git add .
+
+echo "=================================================="
+echo "Running git commit -m "$commit_message" ..."
+echo "=================================================="
 git commit -m "$commit_message"
+
+echo "=================================================="
+echo "Running git push -u origin main ..."
+echo "=================================================="
 git push -u origin main
